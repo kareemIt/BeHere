562288491fc84174b645ddaefa68b26e860eda58
Kareem <kareem.itani2@gmail.com>
2025-03-11 23:11:03 -0400

    endpoint change

--- socialmedia/src/main/java/com/example/socialmedia/Controller/AuthenticationController.java
+++ socialmedia/src/main/java/com/example/socialmedia/Controller/AuthenticationController.java
@@ -17,7 +17,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +28,7 @@ import com.example.socialmedia.service.AuthService;
 import com.example.socialmedia.util.JwtUtil;
 
 @RestController
-@RequestMapping("/api")
+// @RequestMapping("/api")
 public class AuthenticationController {
 
     private final AuthService authService;
